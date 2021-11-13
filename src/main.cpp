b201f7530f70ca9c47bfac196a9ddc53076175dd
lafaerxu <lafaer34@outlook.com>
2021-11-13 23:56:36 +0800

    framework build update

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+
+GTEST_API_ int main(int arg, char*argv[]){
+    testing::InitGoogleTest(&arg,argv);
+    int ret = RUN_ALL_TESTS();
+    return ret;
+}
---
+++ src/reverselist.cpp
@@ -0,0 +1,4 @@
+//
+// Created by frankxu on 2021/11/13.
+//
+
